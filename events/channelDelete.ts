26b3464770e349a371d96ec15fca81a1724b2c53
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-12-05 20:07:17 +1100

    catch error?

--- events/channelDelete.ts
+++ events/channelDelete.ts
@@ -2,25 +2,30 @@ import { GuildChannel } from "discord.js";
 import { deletedPrimary } from "@/lib/operations/primary";
 import { deletedSecondary } from "@/lib/operations/secondary";
 import { prisma } from "@/lib/prisma";
+import { error } from "@/lib/colourfulLogger";
 
 module.exports = {
   name: "channelDelete",
   once: false,
   async execute({ id }: GuildChannel) {
-    const primary = await prisma.primary.findUnique({
-      where: { id },
-    });
-    const secondary = await prisma.secondary.findUnique({
-      where: { id },
-    });
-
-    if (primary) {
-      return prisma.primary.delete({
+    try {
+      const primary = await prisma.primary.findUnique({
         where: { id },
-        include: { aliases: true, secondaries: true },
       });
-    } else if (secondary) {
-      return prisma.secondary.delete({ where: { id } });
+      const secondary = await prisma.secondary.findUnique({
+        where: { id },
+      });
+
+      if (primary) {
+        return prisma.primary.delete({
+          where: { id },
+          include: { aliases: true, secondaries: true },
+        });
+      } else if (secondary) {
+        return prisma.secondary.delete({ where: { id } });
+      }
+    } catch (e) {
+      error(e);
     }
   },
 };
