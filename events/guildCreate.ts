ca6090d03b1959107348546b02f22f9e6bc93f5b
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-12-08 20:45:59 +1100

    add guild check to server join

--- events/guildCreate.ts
+++ events/guildCreate.ts
@@ -1,14 +1,18 @@
 import { prisma } from "../lib/prisma";
 import { Client, Guild } from "discord.js";
 import { debug } from "../lib/colourfulLogger";
+import checkGuild from "../lib/checks/guild";
 
 module.exports = {
   name: "guildCreate",
   once: false,
   async execute(guild: Guild) {
-    await guild.roles.create({
-      name: "Dynamica Manager",
-    });
+    if (!guild.roles.fetch("Dynamica Manager")) {
+      await guild.roles.create({
+        name: "Dynamica Manager",
+      });
+    }
+    checkGuild(guild.id);
     debug(`Joined guild ${guild.id} named: ${guild.name}`);
   },
 };
