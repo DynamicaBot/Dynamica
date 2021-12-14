f1543f98f40bc5b169063901bf8851516e971d5c
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-12-14 22:23:00 +1100

    update to fix literally everything

--- src/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="discord.js" />
-/// <reference types="@discordjs/builders" />
-/// <reference types="@prisma/client" />
--- src/remove-commands.ts
+++ src/remove-commands.ts
@@ -2,17 +2,12 @@ import { config } from "dotenv";
 config();
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
-import * as commands from "./commands";
 
 const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 
 if (!TOKEN || !CLIENT_ID) {
   console.log("Missing env vars.");
 } else {
-  const commandList = Object.values(commands).map((command) =>
-    command.data.toJSON()
-  );
-
   const rest = new REST({ version: "9" }).setToken(TOKEN);
 
   (async () => {
@@ -20,11 +15,11 @@ if (!TOKEN || !CLIENT_ID) {
       console.log("Started refreshing application (/) commands.");
       if (GUILD_ID) {
         await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-          body: commandList,
+          body: [],
         });
       } else {
         await rest.put(Routes.applicationCommands(CLIENT_ID), {
-          body: commandList,
+          body: [],
         });
       }
 
