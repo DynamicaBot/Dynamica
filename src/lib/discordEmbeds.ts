803f174c024441cb4c0d595ae4be4af845d2cb0a
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-12-30 22:42:46 +1100

    use colours to indicate state

--- src/lib/discordEmbeds.ts
+++ src/lib/discordEmbeds.ts
@@ -1,34 +1,22 @@
 import { Embed } from "@discordjs/builders";
 
 export const ErrorEmbed = (message: string) =>
-  new Embed()
-    .setColor(15158332)
-    .setDescription(message)
-    .setTitle("Error")
-    .setAuthor({
-      name: "Dynamica",
-      url: "https://dynamica.dev",
-      iconURL: "https://dynamica.dev/img/dynamica.png",
-    });
+  new Embed().setColor(15158332).setDescription(message).setAuthor({
+    name: "Dynamica",
+    url: "https://dynamica.dev",
+    iconURL: "https://dynamica.dev/img/dynamica.png",
+  });
 
 export const SuccessEmbed = (message: string) =>
-  new Embed()
-    .setColor(3066993)
-    .setDescription(message)
-    .setTitle("Success")
-    .setAuthor({
-      name: "Dynamica",
-      url: "https://dynamica.dev",
-      iconURL: "https://dynamica.dev/img/dynamica.png",
-    });
+  new Embed().setColor(3066993).setDescription(message).setAuthor({
+    name: "Dynamica",
+    url: "https://dynamica.dev",
+    iconURL: "https://dynamica.dev/img/dynamica.png",
+  });
 
 export const InfoEmbed = (message: string) =>
-  new Embed()
-    .setColor(3447003)
-    .setDescription(message)
-    .setTitle("Info")
-    .setAuthor({
-      name: "Dynamica",
-      url: "https://dynamica.dev",
-      iconURL: "https://dynamica.dev/img/dynamica.png",
-    });
+  new Embed().setColor(3447003).setDescription(message).setAuthor({
+    name: "Dynamica",
+    url: "https://dynamica.dev",
+    iconURL: "https://dynamica.dev/img/dynamica.png",
+  });
