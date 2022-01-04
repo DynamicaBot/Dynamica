8cfb0673b055f6ad7aade05ef1b6d3d3d629ea77
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-01-05 08:21:50 +1100

    add reflect metadata

--- src/remove-commands.ts
+++ src/remove-commands.ts
@@ -1,6 +1,7 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import { config } from "dotenv";
+import "reflect-metadata";
 import signale from "signale";
 const { Signale } = signale;
 config();
