2942cf8642acb2378eb5aefb0cfdb4f8689e2c76
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-01-05 09:05:26 +1100

    Forgot to remove some vars from logger

--- src/index.ts
+++ src/index.ts
@@ -24,12 +24,7 @@ const logger = new Signale({
   disabled: false,
   interactive: false,
   logLevel: process.env.LOG_LEVEL || "info",
-  secrets: [
-    process.env.TOKEN,
-    process.env.CLIENT_ID,
-    process.env.GUILD_ID,
-    process.env.DATABASE_URL,
-  ],
+  secrets: [process.env.TOKEN, process.env.CLIENT_ID],
 });
 
 /**
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = "2022.1.4";
+export const version = "2022.1.5";
