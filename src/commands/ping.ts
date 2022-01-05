b3d41fd6e1dc0ad7aa026455c6634ac9e549c754
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-01-06 08:52:33 +1100

    fix missing import

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
-import { Command } from "./command";
+import { Command } from "./";
 
 export const ping: Command = {
   conditions: [],
