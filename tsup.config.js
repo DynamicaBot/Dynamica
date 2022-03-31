218c3533b803a78257214188376d64e6ac0cbba0
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-03-31 13:59:34 +0000

    disable dts

--- tsup.config.js
+++ tsup.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "tsup";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
-  entry: ["src/index.ts", "src/deploy-commands.ts", "src/remove-commands.ts"],
+  entry: ['src/index.ts', 'src/deploy-commands.ts', 'src/remove-commands.ts'],
   splitting: true,
   sourcemap: true,
   clean: true,
-  format: ["esm"],
-  platform: "node",
+  format: ['esm'],
+  platform: 'node',
   minify: !options.watch,
-  tsconfig: "tsconfig.json",
+  tsconfig: 'tsconfig.json',
   bundle: true,
-  dts: !!process.env.TYPE_CHECKING,
   metafile: true,
 }));
