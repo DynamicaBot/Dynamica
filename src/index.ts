19fe33219d758c0d82464f0db6117a0771161d80
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-04-08 00:10:39 +0000

    added cli commands

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import events from '@events';
 import logger from '@utils/logger';
 import { Client, Intents } from 'discord.js';
 import dotenv from 'dotenv';
+import rl from 'readline'
+import deploy from './scripts/deploy';
+import remove from './scripts/remove';
 
 dotenv.config();
 
@@ -17,9 +20,29 @@ const client = new Client({
   ],
 });
 
+(async () => {
+  const readline = rl.createInterface({input: process.stdin, output: process.stdout})
+  for await (const line of readline) {
+    logger.log(line)
+    switch (line) {
+      case "deploy":
+        await deploy()
+        break;
+      case "remove":
+        await remove()
+        break;
+      default:
+        break;
+    }
+  }
+})()
+
+
 const eventList = Object.values(events);
 try {
-  // Register event handlers
+  /**
+   * Register Events
+   */
   eventList.forEach((event) => {
     if (event.once) {
       client.once(event.event, (...args) => event.execute(...args));
@@ -28,6 +51,7 @@ try {
     }
   });
 
+  /** Login */
   client.login(process.env.TOKEN);
 } catch (error) {
   logger.error(error);
---
+++ src/scripts/deploy.ts
@@ -0,0 +1,54 @@
+import commands from '@commands';
+import { REST } from '@discordjs/rest';
+import logger from '@utils/logger';
+import { Routes } from 'discord-api-types/v9';
+import { config } from 'dotenv';
+const { TOKEN, CLIENT_ID, GUILD_ID} = process.env
+
+export default async () => {
+    if (!TOKEN || !CLIENT_ID) {
+        logger.error('Missing env vars.');
+      } else {
+        const importedCommands = Object.values(commands);
+        const commandList = importedCommands.map((command) =>
+          command.commandData.toJSON()
+        );
+      
+        const rest = new REST({ version: '9' }).setToken(TOKEN);
+      
+      
+          try {
+            logger.info(
+              `Started refreshing ${GUILD_ID ? 'guild' : 'application'} (/) commands.`
+            );
+            if (GUILD_ID) {
+              await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+                body: commandList,
+              });
+              logger.debug(
+                'Updated commands: ',
+                commandList.map((c) => c.name).join(', ')
+              );
+            } else {
+              await rest.put(Routes.applicationCommands(CLIENT_ID), {
+                body: commandList,
+              });
+              logger.debug(
+                'Updated commands: ',
+                commandList.map((c) => c.name).join(', ')
+              );
+            }
+      
+            logger.info(
+              `Successfully reloaded ${
+                GUILD_ID ? 'guild' : 'application'
+              } (/) commands.`
+            );
+          } catch (error) {
+            logger.error(error);
+          }
+       
+      }
+}
+
+
