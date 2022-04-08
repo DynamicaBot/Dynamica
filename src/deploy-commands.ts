0dd545db6ad5ba45ec3b4ae4e321e8b4898bb23d
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-04-08 00:18:04 +0000

    added deploy commands back to yarn

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,53 +1,7 @@
-import commands from '@commands';
-import { REST } from '@discordjs/rest';
-import logger from '@utils/logger';
-import { Routes } from 'discord-api-types/v9';
-import { config } from 'dotenv';
-
+import deploy from "./scripts/deploy";
+import { config} from 'dotenv'
 config();
 
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
-
-if (!TOKEN || !CLIENT_ID) {
-  logger.error('Missing env vars.');
-} else {
-  const importedCommands = Object.values(commands);
-  const commandList = importedCommands.map((command) =>
-    command.commandData.toJSON()
-  );
-
-  const rest = new REST({ version: '9' }).setToken(TOKEN);
-
-  (async () => {
-    try {
-      logger.info(
-        `Started refreshing ${GUILD_ID ? 'guild' : 'application'} (/) commands.`
-      );
-      if (GUILD_ID) {
-        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-          body: commandList,
-        });
-        logger.debug(
-          'Updated commands: ',
-          commandList.map((c) => c.name).join(', ')
-        );
-      } else {
-        await rest.put(Routes.applicationCommands(CLIENT_ID), {
-          body: commandList,
-        });
-        logger.debug(
-          'Updated commands: ',
-          commandList.map((c) => c.name).join(', ')
-        );
-      }
-
-      logger.info(
-        `Successfully reloaded ${
-          GUILD_ID ? 'guild' : 'application'
-        } (/) commands.`
-      );
-    } catch (error) {
-      logger.error(error);
-    }
-  })();
-}
+(async () => {
+  await deploy()
+})()
