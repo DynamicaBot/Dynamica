dacd670c332a5de958bc1b6ecb1b438a0aee03a1
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-04-08 00:18:04 +0000

    added deploy commands back to yarn

--- src/remove-commands.ts
+++ src/remove-commands.ts
@@ -1,38 +1,7 @@
-import { REST } from '@discordjs/rest';
-import logger from '@utils/logger';
-import { Routes } from 'discord-api-types/v9';
-import { config } from 'dotenv';
-
+import remove from "./scripts/remove";
+import { config} from 'dotenv'
 config();
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 
-if (!TOKEN || !CLIENT_ID) {
-  logger.error('Missing env vars.');
-} else {
-  const rest = new REST({ version: '9' }).setToken(TOKEN);
-
-  (async () => {
-    try {
-      logger.info(
-        `Started refreshing ${GUILD_ID ? 'guild' : 'application'} (/) commands.`
-      );
-      if (GUILD_ID) {
-        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-          body: [],
-        });
-      } else {
-        await rest.put(Routes.applicationCommands(CLIENT_ID), {
-          body: [],
-        });
-      }
-
-      logger.info(
-        `Successfully deleted ${
-          GUILD_ID ? 'guild' : 'application'
-        } (/) commands.`
-      );
-    } catch (error) {
-      logger.error(error);
-    }
-  })();
-}
+(async () => {
+  await remove()
+})()
