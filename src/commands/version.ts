ee1f9458c98ca2a49c48d77f9e00be4faeba02fc
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2022-04-23 15:20:10 +1000

    Revert "add djs version to /version"
    This reverts commit d2a8aecd1eda33720757213892d812bdaa626270.

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,7 +1,6 @@
 import help from '@/help/version';
 import Command from '@classes/command';
 import { SlashCommandBuilder } from '@discordjs/builders';
-import Discord from 'discord.js';
 
 export default new Command()
   .setCommandData(
@@ -13,6 +12,6 @@ export default new Command()
   .setResponse(async (interaction) => {
     interaction.reply({
       ephemeral: true,
-      content: `The version of the bot is \`${process.env.VERSION}\`.\nThe discord.js version is \`${Discord.version}\`.`,
+      content: `The version of the bot is \`${process.env.VERSION}\`.`,
     });
   });
