f4b2b79cfcfae935730afbeabb352dc761e94820
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2022-04-23 15:20:10 +1000

    Revert "added dynamicachannel class"
    This reverts commit a713d9f1ee2d183c04aef88c7bc7dbfafb30d97c.

--- src/classes/primary.ts
+++ src/classes/primary.ts
@@ -1,15 +1,36 @@
 import db from '@db';
 import Prisma from '@prisma/client';
 import logger from '@utils/logger';
-import { Guild, GuildChannel, User } from 'discord.js';
-import DynamicaChannel from './dynamicaChannel';
+import { Client, Guild, GuildChannel, User, VoiceChannel } from 'discord.js';
 
-export default class DynamicaPrimary extends DynamicaChannel<'primary'> {
+export default class DynamicaPrimary {
   /** The secondary channel as defined by prisma */
-  declare prisma: Prisma.Primary & {
+  prisma: Prisma.Primary & {
     guild: Prisma.Guild;
     secondaries: Prisma.Secondary[];
   };
+
+  /** The discord channel as defined by discord */
+  discord: VoiceChannel;
+
+  /** The discordjs client instance */
+  client: Client<true>;
+
+  /** The channel id as set in fetch */
+  id: string;
+
+  /** The discord guild */
+  discordGuild: Guild;
+
+  /** The prisma guild */
+  prismaGuild: Prisma.Guild;
+
+  constructor(client: Client<true>, channelId?: string) {
+    this.client = client;
+    if (channelId) {
+      this.id = channelId;
+    }
+  }
 
   /**
    * Create a new primary channel
@@ -17,7 +38,11 @@ export default class DynamicaPrimary extends DynamicaChannel<'primary'> {
    * @param user The user who ran the command
    * @param section The section that the channel should be assigned to
    */
-  async create(guild: Guild, user: User, section?: GuildChannel) {
+  async create(
+    guild: Guild,
+    user: User,
+    section?: GuildChannel
+  ): Promise<DynamicaPrimary> {
     try {
       const parent = section?.id;
       const channel = await guild.channels.create('➕ New Session', {
@@ -35,7 +60,7 @@ export default class DynamicaPrimary extends DynamicaChannel<'primary'> {
       logger.debug(
         `New primary channel ${channel.name} created by ${primary.creator}.`
       );
-      this.id = channel.id;
+      this.id = channel.id
       await this.fetch();
     } catch (error) {
       logger.error('Error creating new primary channel:', error);
@@ -43,16 +68,48 @@ export default class DynamicaPrimary extends DynamicaChannel<'primary'> {
     return this;
   }
 
-  async fetchPrisma() {
-    const prisma = await db.primary.findUnique({
+  /**
+   * Fetch the database entry and discord channels (voice and text).
+   * @param channelId The discord channel Id.
+   */
+  async fetch(): Promise<DynamicaPrimary | undefined> {
+    if (!this.client) {
+      throw new Error('No client defined');
+    }
+    if (!this.id) {
+      throw new Error('No id defined');
+    }
+    const primary = await db.primary.findUnique({
       where: { id: this.id },
       include: { guild: true, secondaries: true },
     });
-
-    if (!prisma) {
+    if (!primary) {
       return undefined;
     }
-    return prisma;
+    try {
+      this.prisma = primary;
+      this.prismaGuild = primary.guild;
+    } catch (error) {
+      logger.error('Error fetching primary channel from database:', error);
+    }
+    try {
+      const channel = await this.client.channels.cache.get(this.id);
+      if (!channel) {
+        this.delete();
+        return undefined;
+      }
+      if (!channel.isVoice()) {
+        throw new Error('Not a valid voice channel.');
+      } else if (channel.type === 'GUILD_STAGE_VOICE') {
+        throw new Error('Not a valid voice channel (Stage)');
+      } else {
+        this.discord = channel;
+        this.discordGuild = channel.guild;
+      }
+    } catch (error) {
+      logger.error('Error fetching primary channel from discord:', error);
+    }
+    return this;
   }
 
   /**
@@ -85,13 +142,30 @@ export default class DynamicaPrimary extends DynamicaChannel<'primary'> {
     }
   }
 
-  async deletePrisma(): Promise<void> {
+  private async deletePrisma(): Promise<void> {
     if (!this.id) {
       throw new Error('No id defined.');
     }
-    const prisma = await this.fetchPrisma();
-    if (prisma) {
-      await db.primary.delete({ where: { id: this.id } });
+    await db.primary.delete({ where: { id: this.id } });
+  }
+
+  private async deleteDiscord(): Promise<void> {
+    if (!this.client) {
+      throw new Error('No client defined.');
+    }
+    if (!this.id) {
+      throw new Error('No Id defined.');
+    }
+    try {
+      if (!this.discord.deletable) {
+        throw new Error('The channel is not deletable.');
+      }
+
+      await this.discord.delete();
+
+      logger.debug(`Primary channel deleted ${this.id}.`);
+    } catch (error) {
+      logger.error('Failed to delete primary:', error);
     }
   }
 }
