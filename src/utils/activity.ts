08de89640ed75d0a481a7b601eab289999cd65aa
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-10-09 19:51:05 +0000

    filter presence

--- src/utils/activity.ts
+++ src/utils/activity.ts
@@ -1,13 +1,13 @@
 import { ActivityType, VoiceBasedChannel } from 'discord.js';
 
-const channelActivities = ({ members }: VoiceBasedChannel) =>
-  members
-    .filter((member) => member.presence.activities.length > 0)
+const channelActivities = (channel: VoiceBasedChannel) =>
+  channel?.members
     .filter((member) => !member.user.bot)
+    .filter((member) => !!member.presence)
     .map((member) => member.presence.activities)
     .flat()
     .filter((activity) => activity.type !== ActivityType.Custom)
     .filter((activity) => activity.type !== ActivityType.Listening)
-    .map((activity) => activity.name);
+    .map((activity) => activity.name) ?? [];
 
 export default channelActivities;
